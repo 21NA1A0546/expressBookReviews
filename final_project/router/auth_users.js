aeecee5a18effa6b6f91d6e2f8f194c61d8ce143
21NA1A0546 <21na1a0546.lingayas@limat.edu.in>
2024-08-12 11:31:18 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,20 +10,81 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  if (!username || !password) {
+    return false
+  }
+
+  const user = users.find(u => u.username === username) 
+  if (!user) {
+    return false
+  }
+
+  if (user.password !== password) {
+    return false
+  }
+
+  return true
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username
+  const password = req.body.password
+
+  const secretKey = 'access'
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      { data: username}, secretKey,
+      { expiresIn: 60 * 60 }
+    )
+
+    req.session.authorization = { accessToken, username  }
+    return res.status(200).json({message: "Succsesfully login"});
+  }
+  
+  return res.status(401).json({message: "Failed to login"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn
+  const review = req.body.review
+  const book = books[isbn]
+  if (!book) {
+    return res.status(404).json({message: 'Book is not found'})
+  }
+
+  const user = req.user.data
+
+  let reviews = book.reviews || {}
+  reviews[user] = review
+  book.reviews = reviews
+  books[isbn] = book  
+  
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json({book})
 });
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn
+  const user = req.user.data
+
+  const book = books[isbn]
+  
+  if (!book) {
+    return res.status(404).json({message: 'Book is not found'})
+  }
+
+  if (!book.reviews || !book.reviews[user]) {
+    return res.status(204).json({ message: "Review successfully deleted"})
+  }
+
+  delete books[isbn].reviews[user]
+  return res.status(204).json({ message: "Review successfully deleted" })
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
