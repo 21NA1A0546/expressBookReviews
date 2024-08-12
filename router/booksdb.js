05ec8e4f6cc8f1ece702b2036536b22517ff5359
21NA1A0546 <21na1a0546.lingayas@limat.edu.in>
2024-08-12 05:28:06 -0400

    Completed general user routes and tested with Postman

---
+++ router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+      4: {"author": "Sîn-lēqi-unninni","title": "The Epic Of Gilgamesh", "reviews": {} },
+      5: {"author": "Moses","title": "The Book Of Job", "reviews": {} },
+      6: {"author": "J.C.Mardrus","title": "One Thousand and One Nights", "reviews": {} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"guest": "testing"} }
+}
+
+module.exports=books;
